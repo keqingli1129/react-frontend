8949e11f6ced62ca21df03d107df1e5d308783f7
K L <keqingli1129@gmail.com>
2022-05-09 16:14:21 -0500

    React FrontEnd

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { Navbar } from './Navbar'
+
+class HeaderComponent extends Component {
+
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         
+      }
+    }
+  render() {
+    return (
+        <Navbar>  </Navbar>
+    )
+  }
+}
+
+export default HeaderComponent
---
+++ src/components/test.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export const test = () => {
+  return (
+    <div>test</div>
+  )
+}
+import React from 'react'
+
+const test = () => {
+  return (
+    <div>test</div>
+  )
+}
+
+export default test
+
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const test = props => {
+  return (
+    <div>test</div>
+  )
+}
+
+test.propTypes = {}
+
+export default test
+
+raf
